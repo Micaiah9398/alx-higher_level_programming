ca2d9d1a8a0acc35c01c5df7a483adbd2b37e510
micaiah <micaiahadamson1993@gmail.com>
2023-03-10 05:09:45 -0800

    variables added and imported

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+f __name__ == "__main__":
+        from calculator_1 import add, sub, mul, div
+            a = 10
+            b = 5
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))
