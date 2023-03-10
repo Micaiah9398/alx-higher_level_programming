5fd203e3afead95f87ceb905690164242360c8de
micaiah <micaiahadamson1993@gmail.com>
2023-03-10 11:43:26 -0800

    arguments imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 def print_arg(argv):
 n = len(argv) -1
 if n == 0:
-    print("{:d} argument.".format(n))
+    print("{:d} argument:".format(n))
     return
 else:
     if n == 1:
@@ -12,7 +12,7 @@ else:
     i = 1
     while i <= n:
         print("{:d}: {:s}".format(i, argv[i]))
-        i += 1
+        i += 1 
 
         if __name__ == "__main__":
             import sys
