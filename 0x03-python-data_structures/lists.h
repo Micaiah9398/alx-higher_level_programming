d3ac2fd490de4f6e323efd6c848c5b61a2113c7e
micaiah <micaiahadamson1993@gmail.com>
2023-03-13 21:55:29 -0700

    data structures

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,17 +1,26 @@
-#ifndef _"LIST"_
-#define _"LIST"_
-def print_list_integer(my_list=[]):
-def element_at(my_list, idx):
-def replace_in_list(my_list, idx, element):
-def print_reversed_list_integer(my_list=[]):
-def new_in_list(my_list, idx, element):
-def no_c(my_string):
-def print_matrix_integer(matrix=[[]]):
-def add_tuple(tuple_a=(), tuple_b=()):
-def multiple_returns(sentence):
-def max_integer(my_list=[]):
-def divisible_by_2(my_list=[]):
-def delete_at(my_list=[], idx=0):
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ *  * struct listint_s - singly linked list
+ *   * @n: integer
+ *    * @next: points to the next node
+ *     *
+ *      * Description: singly linked list node structure
+ *       * for ALX project
+ *        */
+typedef struct listint_s
+{
+		int n;
+			struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
-#endif
+
+#endif /* LISTS_H */
