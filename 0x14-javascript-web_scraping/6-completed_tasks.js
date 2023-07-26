a1dddd583491582a11a273f368384de764128111
Micaiah <micaiahadamson1993@gmail.com>
2023-07-26 07:09:09 -0700

    updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,26 +5,22 @@ const url = process.argv[2];
 
 request(url, function (err, response, body) {
   if (err) {
-    console.log('Error:', err);
+    console.log(err);
   } else if (response.statusCode === 200) {
-    const employeeId = parseInt(process.argv[3]);
-    const todos = JSON.parse(body);
-
-    // Filter completed tasks for the given employee ID
-    const completedTasks = todos.filter(task => task.completed && task.userId === employeeId);
-    const totalTasks = todos.filter(task => task.userId === employeeId);
-
-    const employeeName = completedTasks.length > 0 ? completedTasks[0].name : '';
-    const numberOfCompletedTasks = completedTasks.length;
-    const totalNumberOfTasks = totalTasks.length;
-
-    console.log(`Employee ${employeeName} is done with tasks(${numberOfCompletedTasks}/${totalNumberOfTasks}):`);
-
-    for (const task of completedTasks) {
-      console.log(`\t${task.title}`);
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
     }
+    console.log(completed);
   } else {
-    console.log('An error occurred. Status code:', response.statusCode);
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
-
