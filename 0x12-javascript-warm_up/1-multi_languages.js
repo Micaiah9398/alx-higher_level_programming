1c81ab433157a8b4f7ddee5644707549a762590e
Micaiah <micaiahadamson1993@gmail.com>
2023-07-27 02:44:01 -0700

    updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const myVar = "c is fun";
-const myVar_2 = "Python is cool";
-const myVar_3 = "JavaScript is amazing";
-console.log(myVar, myVar_2, myVar_3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
