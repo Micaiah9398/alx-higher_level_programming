9b385feb7b33e358da649832096893bc8a5a0daa
Micaiah <micaiahadamson1993@gmail.com>
2023-08-07 20:35:45 -0700

    jquery

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // When the document is ready, the event listener will be added
+  $('#red_header').click(function() {
+    // Add the "red" class to the <header> element
+    $('header').addClass('red');
+  });
+});
+
