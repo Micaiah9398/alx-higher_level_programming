ce057326eea1fbb7708e1c3211946340736ce619
Michaddy <micaiahadamson1993@gmail.com>
2023-08-17 08:46:27 -0700

    corrected

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-'''print alphabet in lowercase, not followed by a newline'''
-for letter in range(97,123):
-    print("{}".format(chr(letter))end="")
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
